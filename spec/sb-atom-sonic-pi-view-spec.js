7198bc376bd2a1dfe253dcff85c7f912726197b2
SunderB <20426079+SunderB@users.noreply.github.com>
2018-05-31 09:54:51 +0100

    Convert coffeescript files to javascript files The old coffeescript files have been placed in 'coffee' subfolders for now. TODO: Double check code for JS errors, remove decaffeinate suggestions, remove coffeescript files.

---
+++ spec/sb-atom-sonic-pi-view-spec.js
@@ -0,0 +1,11 @@
+/*
+ * decaffeinate suggestions:
+ * DS102: Remove unnecessary code created because of implicit returns
+ * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
+ */
+const sbAtomSonicPiView = require('../lib/sb-atom-sonic-pi-view');
+
+describe("sbAtomSonicPiView", () =>
+  it("has one valid test", () => expect("life").toBe("easy"))
+);
+// I'm not sure why this test is here, but OK...
